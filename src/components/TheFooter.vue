44026bfec7f36741bdeeddd4b380624498bb981d
fcristinadebora <fcristinadebora@gmail.com>
2023-01-07 13:07:08 -0300

    footer done

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,22 +1,22 @@
 <template>
-  <footer class="footer mt-auto py-2 bg-custom-secondary w-100">
-    <div class="footer_detail d-flex">
-      <nav class="">
+  <footer class="footer mt-auto w-100">
+    <div class="container footer_detail d-flex flex-row flex-sm-row justify-content-between py-3">
+      <div class="d-flex flex-column align-items-center col-5 col-sm-auto" id="footer-left">
         <img
           src="public/img/logo_blue.png"
-          class="footer_detail_logo"
+          class="footer_detail_logo mb-2"
           alt="Logo"
         />
-        <span class="text">Conectando produtores e consumidores locais</span>
-        <div class="social-icons">
-          <ul class="navbar-nav">
-            <li><i class="bi bi-instagram"></i></li>
-            <li><i class="bi bi-twitter"></i></li>
-          </ul>
-        </div>
-      </nav>
-      <div class="content-right">
-        <ul>
+        <span class="text mb-2">Conectando produtores e consumidores locais</span>
+        <nav class="social-icons">
+            <ul class="navbar-nav flex-row footer_detail_nav">
+              <li class="me-2"><a href=""><i class="bi bi-instagram"></i></a></li>
+              <li><a href=""><i class="bi bi-twitter"></i></a></li>
+            </ul>
+        </nav>
+      </div>
+      <nav id="footer_right" class="col-8 col-sm-auto">
+        <ul class="navbar-nav color-secondary footer_detail_nav justify-content-between">
           <li>
             <a href="">Sobre nós</a>
           </li>
@@ -30,9 +30,10 @@
             <a href="">Política de privacidade</a>
           </li>
         </ul>
-      </div>
+
+      </nav>
     </div>
-    <div class="footer_copyright bg-custom-secondary">
+    <div class="footer_copyright bg-custom-secondary py-2">
       <div class="container text-center">
         <span class="color-white"
           >Ⓒ 2022 - Locavore - Todos os direitos reservados</span
@@ -42,7 +43,28 @@
   </footer>
 </template>
 
-<style>
+<style lang="scss">
+@import "@/assets/scss/_colors";
+.footer_detail{
+  text-align: center;
+}
+
 .footer_detail_logo {
+  width: 150px;
+}
+
+.footer_detail_nav {
+  li {
+    margin-bottom: 0.2rem;
+  }
+
+  a {
+    color: $color-brand-secondary;
+    text-decoration: none;
+    
+    &:hover {
+      color: $color-brand-secondary;
+    }
+  }
 }
 </style>
