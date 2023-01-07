bfa1a7a6c3d44d6d715cab02bbba4310b854570e
Debora Cristina Fontanella <debora.fontanella@barkyn.com>
2023-01-07 14:38:57 +0000

    start footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,48 @@
 <template>
   <footer class="footer mt-auto py-2 bg-custom-secondary w-100">
-    <div class="container text-center">
-      <span class="color-white"
-        >Ⓒ 2022 - Locavore - Todos os direitos reservados</span
-      >
+    <div class="footer_detail d-flex">
+      <nav class="">
+        <img
+          src="public/img/logo_blue.png"
+          class="footer_detail_logo"
+          alt="Logo"
+        />
+        <span class="text">Conectando produtores e consumidores locais</span>
+        <div class="social-icons">
+          <ul class="navbar-nav">
+            <li><i class="bi bi-instagram"></i></li>
+            <li><i class="bi bi-twitter"></i></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="content-right">
+        <ul>
+          <li>
+            <a href="">Sobre nós</a>
+          </li>
+          <li>
+            <a href="">Contato</a>
+          </li>
+          <li>
+            <a href="">Perguntas frequentes</a>
+          </li>
+          <li>
+            <a href="">Política de privacidade</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer_copyright bg-custom-secondary">
+      <div class="container text-center">
+        <span class="color-white"
+          >Ⓒ 2022 - Locavore - Todos os direitos reservados</span
+        >
+      </div>
     </div>
   </footer>
 </template>
+
+<style>
+.footer_detail_logo {
+}
+</style>
