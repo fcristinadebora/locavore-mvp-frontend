43c66fda2965e260aa989baed1e85a9ea8fe0e0e
Debora Fontanella <debora.fontanella@inventsys.ch>
2023-06-14 19:28:06 +0100

    fix description

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -49,7 +49,7 @@ onMounted(async () => {
         <i class="bi bi-image icon-lg" v-if="!product.image"></i>
       </div>
 
-      <p class="w-100 mb-3 mt-3 color-default" v-html="product.description.replaceAll('\n', '<br>')"></p>
+      <p class="w-100 mb-3 mt-3 color-default" v-html="product.description?.replaceAll('\n', '<br>')"></p>
 
       <p class="color-primary w-100">
         {{ toMoney(product.price) || '' }}<span v-if="product.unit_of_price">/{{ product.unit_of_price }}</span>
